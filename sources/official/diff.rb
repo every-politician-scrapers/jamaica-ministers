ee2c610f7899b62f4320aa4e1e864871b25ddc94
Tony Bowden <tony@tmtm.com>
2022-05-22 12:59:09 +0300

    include Ministers of State

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -4,5 +4,4 @@
 require 'every_politician_scraper/comparison'
 
 diff = EveryPoliticianScraper::Comparison.new('wikidata.csv', 'scraped.csv').diff
-                                         .reject { |row| row.last.to_s.include? 'Minister of State' }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
